617e9edcd1a617ef63dfac1ef3f86ae01be1d59c
glopes <glopesdev@users.noreply.github.com>
2011-04-23 16:45:10 +0100

    First draft of OpenOrtho.

---
+++ OpenOrtho/CephalometricAnalysis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace OpenOrtho
+{
+    public class CephalometricAnalysis
+    {
+        private readonly List<Vector2> points = new List<Vector2>();
+
+        public List<Vector2> Points
+        {
+            get { return this.points; }
+        }
+    }
+}
